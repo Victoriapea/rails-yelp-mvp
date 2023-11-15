95338fca22225623228aaefa1b4e25c849fa1a0e
Victoria Pea <victoriapea@outlook.fr>
2023-11-15 17:05:52 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french", phone_number: 0623456547}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "chinese", phone_number: 1111111112}
+beesket = {name: "Beesket", address: "20A Shoreditch High St, London E1 6PQ", category: "italian", phone_number: 0623656532}
+macdo = {name: "Macdo", address: "30 Shoreditch High St, London E1 6PQ", category: "japenese", phone_number: 1111111111}
+pizza_hut = {name: "Pizza Hut", address: "40 Shoreditch High St, London E1 6PQ", category: "belgian", phone_number: 0623456574}
+
+[dishoom, pizza_east, beesket, macdo, pizza_hut].each do |resto|
+  restaurant = Restaurant.create(resto)
+  puts restaurant.name
+end
+
+puts "Finished!"
